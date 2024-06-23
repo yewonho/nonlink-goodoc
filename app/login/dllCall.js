d82b2d6b479eb6c875accffb58c4f330dc64cd1f
unicorn_goodoc <unicorn_goodoc@goodoc.co.kr>
2024-06-23 22:05:11 +0900

    Revert "집커밋"
    This reverts commit 60bc07da4d3ddf6effbd09b844f7d7b2376aa95f.

--- app/login/dllCall.js
+++ app/login/dllCall.js
@@ -8,111 +8,80 @@ export default function Home() {
     const [data, setData] = useState(null);
     const [error, setError] = useState(null);
 
-    useEffect(() => {
-        async function fetchData() {
-            try {
-               console.log('트라이 시작');
-                const response = await fetch('../api/post/bridgeInit', {
-                    method: 'POST',
-                    headers: {
-                      'Content-Type': 'application/json'
-                    },
-                    body: JSON.stringify({
-                      methodName: 'EdgeGdlInit', // 호출할 메서드 이름 EdgeGdlLogin , EdgeGdlInit, EdgeGdlDeInit, EdgeGdlSendRequest
-                      payload: {
-                         data: {
-                        //   cellphone : parseInt(cellphone, 10),
-                        //   regNum:parseInt(regNum, 10),
-                        //   eventId:parseInt(eventId, 10),
-                        //   jobId:String(jobId)},
-                        //  length: parseInt(length, 10)
-
-
-
-                        // 로그인용
-                        // hospitalCode: 20000351,
-                        // userId: 'e20000351',
-                        // password: 'goodoc123!'
-
-
-
-                        //init용
-                                  chartCode: 3350,
-                                  hospitalCode: '20000351',
-                                  initType: 0
-                      }
-                    }})
+//     useEffect(() => {
+//         async function fetchData() {
+//             try {
+//                 const response = await fetch('../api/post/bridgeInit', {
+//                     method: 'GET',
                    
-                });
+//                 });
 
-                if (!response.ok) {
-                  console.log('리스펀스 에러'+response.ok);
-                  throw new Error('Network response was not ok');
-                }
+//                 if (!response.ok) {
+//                     throw new Error('Network response was not ok');
+//                 }
 
-                const result = await response.json();
-                setData(result);
-            } catch (error) {
-                setError(error.message);
-            }
-        }
+//                 const result = await response.json();
+//                 setData(result);
+//             } catch (error) {
+//                 setError(error.message);
+//             }
+//         }
 
-        fetchData();
-    }, []);
+//         fetchData();
+//     }, []);
 
-    if (error) {
-      return <div>Error: {error.message}</div>;
-    }
-  
-    if (!data) {
-      return <div>Loading...</div>;
-    }
-  
-    return (
-      <div>
-        <h1>DLL Method Result</h1>
-        <pre>{JSON.stringify(data, null, 2)}</pre>
-      </div>
-    );
-  }
-
-
-
-// dll 로드 정상적으로 되는거. 
-// const callDllMethod = async () => {
-//     try {
-//       const res = await fetch('../api/post/bridgeInit', {
-//         method: 'POST',
-//         headers: {
-//           'Content-Type': 'application/json',
-//         },
-//         body: JSON.stringify({
-//           chartCode: 3350,
-//           hospitalCode: '20000351',
-//           initType: 0
-//         }),
-//       });
-
-//       const data = await res.json();
-//       if (res.ok) {
-//         console.log(data.result)
-//         setData(data.result);
-//       } else {
-//         console.log(data.result)
-//         setError(data.error);
-//       }
-//     } catch (err) {
-//       setError('Failed to call API');
+//     if (error) {
+//         return <div>Error: {error}</div>;
 //     }
-//   };
 
-//   return (
-//     <div>
-//       <h1>DLL Method Caller</h1>
-//       <button onClick={callDllMethod}>Call DLL Method</button>
-//       {data && <pre>{JSON.stringify(data, null, 2)}</pre>}
-//       {error && <pre style={{ color: 'red' }}>{error}</pre>}
-//     </div>
-//   );
+//     if (!data) {
+//         return <div>Loading...</div>;
+//     }
 
-//}
+//     return (
+//         <div>
+//             <h1>DLL Load Result</h1>
+//             <pre>{JSON.stringify(data, null, 2)}</pre>
+//         </div>
+//     );
+// }
+
+
+
+const callDllMethod = async () => {
+    try {
+      const res = await fetch('../api/post/bridgeInit', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          chartCode: 3350,
+          hospitalCode: '20000351',
+          initType: 0
+        }),
+      });
+
+      const data = await res.json();
+      if (res.ok) {
+        console.log(data.result)
+        setData(data.result);
+      } else {
+        console.log(data.result)
+        setError(data.error);
+      }
+    } catch (err) {
+      setError('Failed to call API');
+    }
+  };
+
+  return (
+    <div>
+      <h1>DLL Method Caller</h1>
+      <button onClick={callDllMethod}>Call DLL Method</button>
+      {data && <pre>{JSON.stringify(data, null, 2)}</pre>}
+      {error && <pre style={{ color: 'red' }}>{error}</pre>}
+    </div>
+  );
+
+}
